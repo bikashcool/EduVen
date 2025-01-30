5e0c43c8b45ece212bde493d46bbef1c214f28b5
BikashKumar <bikashcool481@gmail.com>
2025-01-31 00:00:31 +0530

    Initial Commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       {/* Section-1 */}
-      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-center">
+      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-center max-w-maxContent">
         <Link to={"/signup"}>
           <div className="mx-auto mt-16 p-2 rounded-xl bg-richblack-800 font-bold text-richblack-100 transition-all duration-150 hover:scale-95 w-fit">
             <div className="flex items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-150 group-hover:bg-richblack-900">
