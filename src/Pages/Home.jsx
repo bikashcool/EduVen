9f73282cc496de6df12992155e126240f2eea7b6
BikashKumar <bikashcool481@gmail.com>
2025-01-31 23:53:04 +0530

    add section data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FaArrowRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import CTAButton from "../Components/Core/HomePage/Button";
 import HighLightText from '../Components/Core/HomePage/HighLightText';
-
+import Banner from '../assets/Images/banner.mp4';
+import CodeBlocks from '../Components/Core/HomePage/CodeBlocks';
 const Home = () => {
   return (
     <div>
@@ -21,11 +23,84 @@ const Home = () => {
           Empower Your Future with <HighLightText text={"Coding Skills"} />
         </div>
 
-        <div className='mt-4 w-[90%] text-center text-base font-bold text-richblack-400'>
+        <div className="mt-4 w-[90%] text-center text-base font-bold text-richblack-400">
           With our online coding courses, you can learn at your own pace, from
           anywhere in the world, and get access to a wealth of resources,
           including hands-on projects, quizzes, and personalized feedback from
           instructors.
+        </div>
+
+        <div className="flex gap-7 mt-8">
+          <CTAButton active={true} linkto={"/signup"}>
+            Learn More
+          </CTAButton>
+
+          <CTAButton active={false} linkto={"/login"}>
+            Book a Demo
+          </CTAButton>
+        </div>
+
+        <div className="mx-3 my-12 w-[70%] relative shadow-blue-200">
+          <video muted loop autoPlay>
+            <source src={Banner} type="video/mp4" />
+          </video>
+        </div>
+
+        {/* Code Section 1 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row"}
+            heading={
+              <div className="text-2xl font-semibold lg:text-4xl sm:w-full">
+                Unlock Your
+                <HighLightText text={" Coding Potential "} />
+                with our online courses
+              </div>
+            }
+            subHeading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "Try it Yourself",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
+            codeColor={"text-richblack-200"}
+          />
+        </div>
+
+        {/* Code Section 2 */}
+        <div>
+          <CodeBlocks
+            position={"lg: flex-row-reverse"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Start
+                <HighLightText text={" Coding in Seconds "} />
+              </div>
+            }
+            subHeading={
+              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+            }
+            ctabtn1={{
+              btnText: "Continue Lesson",
+              linkto: "/signup",
+              active: false,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              linkto: "/login",
+              active: true,
+            }}
+            codeblock={`<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
+            codeColor={"text-richblack-200"}
+          />
         </div>
       </div>
       {/* Section-2 */}
