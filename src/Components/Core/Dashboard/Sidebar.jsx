5cce125349b3e8ac7b1ba34c39584de7e5e68b84
BikashKumar <bikashcool481@gmail.com>
2025-03-01 01:37:26 +0530

    add sidebarLinks and bit change in sidebar

--- src/Components/Core/Dashboard/Sidebar.jsx
+++ src/Components/Core/Dashboard/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
 
   return (
     <div>
-      <div className='flex h-[calc(100vh-3.5rem)] min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-100 py-10'>
+      <div className='flex h-[calc(100vh-3.5rem)] min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10'>
         <div className='flex flex-col'>
             {
                 sidebarLinks.map((link) => {
